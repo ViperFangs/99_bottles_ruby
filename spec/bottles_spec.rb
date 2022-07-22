69d83af2bf145867c752ae5ca2ce6a27db243e39
Aarya <amathrej@asu.edu>
2022-07-22 02:03:58 -0700

    Write tests for 98th verse

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -8,9 +8,9 @@ describe Bottles do
       subject(:verse) { described_class.new.verse(99) }
 
       it 'returns the 99th verse' do
-        verse_99_result = '99 bottles of beer on the wall, ' \
-                          "99 bottles of beer.\n" \
-                          'Take one down and pass it around, ' \
+        verse_99_result = '99 bottles of beer on the wall, ' +
+                          "99 bottles of beer.\n" +
+                          'Take one down and pass it around, ' +
                           "98 bottles of beer on the wall.\n"
 
         expect(verse).to eq verse_99_result
