eb71da67f9bcc5f19cccd89c6f25cbc30a2f84c8
Aarya <amathrej@asu.edu>
2022-07-22 02:04:40 -0700

    Write code for 99th verse Only writing enough code to pass the tests

---
+++ lib/bottles.rb
@@ -0,0 +1,9 @@
+# Bottles is an interface for 99 bottles song
+class Bottles
+  def verse(number)
+    '99 bottles of beer on the wall, ' +
+    "99 bottles of beer.\n" +
+    'Take one down and pass it around, ' +
+    "98 bottles of beer on the wall.\n"
+  end
+end
