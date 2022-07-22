7c7544b10ec1b6208035d67102341d646d19e4fd
Aarya <amathrej@asu.edu>
2022-07-22 02:09:40 -0700

    Fix test for 98th verse

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -5,7 +5,7 @@ require_relative '../lib/bottles'
 describe Bottles do
   describe '#verse' do
     context 'Unit testing to check if #verses returns a specified verse' do
-      subject(:verse) { described_class.new.verse(99) }
+      subject(:bottles) { described_class.new }
 
       it 'returns the 99th verse' do
         verse_99_result = '99 bottles of beer on the wall, ' +
@@ -13,16 +13,16 @@ describe Bottles do
                           'Take one down and pass it around, ' +
                           "98 bottles of beer on the wall.\n"
 
-        expect(verse).to eq verse_99_result
+        expect(bottles.verse(99)).to eq verse_99_result
       end
 
       it 'returns the 98th verse' do
-        verse_99_result = '99 bottles of beer on the wall, ' +
-                          "99 bottles of beer.\n" +
+        verse_98_result = '98 bottles of beer on the wall, ' +
+                          "98 bottles of beer.\n" +
                           'Take one down and pass it around, ' +
-                          "98 bottles of beer on the wall.\n"
+                          "97 bottles of beer on the wall.\n"
 
-        expect(verse).to eq verse_99_result
+        expect(bottles.verse(98)).to eq verse_98_result
       end
     end
   end
