9f3960255f8f17df19e4d3137fd329998683a49e
Aarya <amathrej@asu.edu>
2022-07-22 02:17:28 -0700

    Modify #verses to return verse 3 to 99

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,17 @@
 # Bottles is an interface for 99 bottles song
 class Bottles
   def verse(number)
-    '99 bottles of beer on the wall, ' +
-    "99 bottles of beer.\n" +
-    'Take one down and pass it around, ' +
-    "98 bottles of beer on the wall.\n"
+    case number
+    when 99
+      '99 bottles of beer on the wall, ' +
+      "99 bottles of beer.\n" +
+      'Take one down and pass it around, ' +
+      "98 bottles of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      'Take one down and pass it around, ' +
+      "#{number - 1} bottles of beer on the wall.\n"
+    end
   end
 end
