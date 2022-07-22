d345073bbd81ee89fa0aa1129c0338f3b17892a6
Aarya <amathrej@asu.edu>
2022-07-22 02:05:37 -0700

    Write tests for 98th verse

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -15,6 +15,15 @@ describe Bottles do
 
         expect(verse).to eq verse_99_result
       end
+
+      it 'returns the 98th verse' do
+        verse_99_result = '99 bottles of beer on the wall, ' +
+                          "99 bottles of beer.\n" +
+                          'Take one down and pass it around, ' +
+                          "98 bottles of beer on the wall.\n"
+
+        expect(verse).to eq verse_99_result
+      end
     end
   end
 end
