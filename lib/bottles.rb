0a8927163cc4de7b1c93acccc031822df7f7ce04
Aarya <amathrej@asu.edu>
2022-09-09 00:38:05 -0700

    Implement code for test 3

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,6 +5,10 @@ class Bottles
       "3 bottles of beer on the wall, 3 bottles of beer.
 Take one down and pass it around, 2 bottles of beer on the wall.
 "
+  elsif number == 2
+    "2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+"
     else
     "99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
