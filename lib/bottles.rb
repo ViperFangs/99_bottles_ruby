8ce97d058339e929c420cb5f348823771fad7f98
Aarya <amathrej@asu.edu>
2022-09-09 00:39:49 -0700

    Write code for test 4

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,6 +9,10 @@ Take one down and pass it around, 2 bottles of beer on the wall.
     "2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
 "
+  elsif number == 1
+    "1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+"
     else
     "99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
