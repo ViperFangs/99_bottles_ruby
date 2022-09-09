e449e1376b0333e013138dcf782b1cd58fd9fb1b
Aarya <amathrej@asu.edu>
2022-09-09 00:36:00 -0700

    Write code for test 2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,14 @@
 class Bottles
 
-  def verse(_)
+  def verse(number)
+    if number == 3
+      "3 bottles of beer on the wall, 3 bottles of beer.
+Take one down and pass it around, 2 bottles of beer on the wall.
+"
+    else
     "99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
 "
+    end
   end
 end
